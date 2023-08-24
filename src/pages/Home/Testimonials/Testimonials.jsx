3072f588a5095bc9b9fd7f84f2225f15e9fd480b
aothymoon59 <mmoon8745@gmail.com>
2023-08-25 02:13:44 +0600

    testimonial section done

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -15,19 +15,27 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([])
 
     useEffect(() => {
-        fetch('/reviews.json')
+        fetch(`${import.meta.env.VITE_API_URL}/reviews`)
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
+
+    const breakpoints = {
+        // Otherwise, show 2 slides per view
+        1100: {
+            slidesPerView: 2,
+        },
+    };
+
     return (
         <section className='pt-8 md:pt-[130px]'>
             <Container>
                 <div className='text-center max-w-4xl mx-auto mb-[50px]'>
-                    <h2 className='text-[40px] font-bold'>What Our Patients Says</h2>
+                    <h2 className='text-[30px] sm:text-[40px] font-bold'>What Our Patients Says</h2>
                     <p className='text-[#3B3A3A]'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
                 </div>
                 <Swiper
-                    slidesPerView={2}
+                    slidesPerView={1}
                     spaceBetween={24}
                     navigation={true}
                     loop={true}
@@ -37,23 +45,25 @@ const Testimonials = () => {
                         disableOnInteraction: false,
                     }}
                     className="mySwiper"
+                    breakpoints={breakpoints}
                 >
                     {reviews.map((review) => (
                         <SwiperSlide key={review?._id}>
                             <div className="p-[50px] border border-[#E6E6E6] rounded-[10px]">
                                 <div className='flex justify-between items-center mb-5'>
                                     <div className='flex items-center gap-5'>
-                                        <img className="w-[66px] h-[66px] rounded-full border border-[#F7A582] object-cover object-center" src={review?.image} alt={review?.name} />
+                                        <img className="w-[50px] md:w-[66px] h-[50px] md:h-[66px] rounded-full border border-[#F7A582] object-cover object-center" src={review?.image} alt={review?.name} />
                                         <div>
-                                            <h2 className='text-xl font-bold'>{review?.name}</h2>
-                                            <h4 className='text-[#6C6B6B]'>{review?.title}</h4>
+                                            <h2 className='text-lg sm:text-xl font-bold'>{review?.name}</h2>
+                                            <h4 className='text-sm sm:text-base text-[#6C6B6B]'>{review?.title}</h4>
                                         </div>
                                     </div>
-                                    <div><FaQuoteLeft className="text-2xl sm:text-4xl lg:text-[54px] text-[#F7A582]" /></div>
+                                    <div className='hidden md:block'><FaQuoteLeft className="text-4xl lg:text-[54px] text-[#F7A582]" /></div>
                                 </div>
                                 <div className=''>
-                                    <p className='text-lg text-[#6C6B6B]'>{review?.description}</p>
+                                    <p className='text-base sm:text-lg text-[#6C6B6B]'>{review?.description}</p>
                                 </div>
+                                <div className='block md:hidden mt-5'><FaQuoteLeft className="text-4xl lg:text-[54px] text-[#F7A582]" /></div>
                             </div>
                         </SwiperSlide>
                     ))}
